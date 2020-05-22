727a1168efc14ba62ba7d03f6660527fc819767e
YunZhong <1125213018@qq.com>
2020-05-22 19:54:00 +0800

    Initial commit

---
+++ app/src/main/java/com/chetu/user/MyApplication.java
@@ -0,0 +1,102 @@
+package com.chetu.user;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+
+import com.hjq.toast.ToastUtils;
+import com.mob.MobSDK;
+import com.tencent.bugly.crashreport.CrashReport;
+
+/**
+ * Created by zyz on 2018/1/18.
+ */
+
+public class MyApplication extends Application {
+    // 上下文菜单
+    private static Context mContext;
+
+    private static MyApplication myApplication;
+
+    public static MyApplication getInstance() {
+        return myApplication;
+    }
+
+    @Override
+    public final void onCreate() {
+        super.onCreate();
+
+        mContext = this;
+        myApplication = this;
+        //推送初始化
+        MobSDK.init(getApplicationContext());
+        //控制用户隐私授权的结果
+        MobSDK.submitPolicyGrantResult(true, null);
+
+        //腾讯bugly 异常上报初始化-建议在测试阶段建议设置成true，发布时设置为false。
+        CrashReport.initCrashReport(getApplicationContext(), "5ee0dd5813", false);
+
+        /*//非wifi情况下，主动下载x5内核
+        QbSdk.setDownloadWithoutWifi(false);
+        //搜集本地tbs内核信息并上报服务器，服务器返回结果决定使用哪个内核。
+        QbSdk.PreInitCallback cb = new QbSdk.PreInitCallback() {
+            @Override
+            public void onViewInitFinished(boolean arg0) {
+                //x5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。
+                MyLogger.i("5內核初始化完成的回调，为true表示x5内核加载成功，否则表示x5内核加载失败，会自动切换到系统内核。" + arg0);
+            }
+
+            @Override
+            public void onCoreInitFinished() {
+
+            }
+        };
+        //x5内核初始化接口
+        QbSdk.initX5Environment(getApplicationContext(), cb);*/
+
+        //toast初始化
+        ToastUtils.init(this);
+
+        /**
+         * 对于7.0以下，需要在Application创建的时候进行语言切换
+         */
+        /*String language = SpUtil.getInstance(this).getString(SpUtil.LANGUAGE);
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            LanguageUtil.changeAppLanguage(mContext, language);
+        }
+
+        new ScreenAdaptation(this, 828, 1792).register();
+//        new ScreenAdaptation(this,720,1280).register();*/
+
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+//        MultiDex.install(this);//方法数超过64k
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        if (newConfig.fontScale != 1)//非默认值
+            getResources();
+        super.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public Resources getResources() {
+        Resources res = super.getResources();
+        if (res.getConfiguration().fontScale != 1) {//非默认值
+            Configuration newConfig = new Configuration();
+            newConfig.setToDefaults();//设置默认
+            res.updateConfiguration(newConfig, res.getDisplayMetrics());
+        }
+        return res;
+    }
+}
---
+++ app/src/main/java/com/chetu/user/activity/MainActivity.java
@@ -0,0 +1,30 @@
+package com.chetu.user.activity;
+
+import android.os.Bundle;
+
+import com.chetu.user.R;
+import com.chetu.user.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void updateView() {
+
+    }
+}
